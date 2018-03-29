bdbb99c7dd7facae14f56254ea85f3586935d2c2
tans0348 <37565114+tans0348@users.noreply.github.com>
2018-03-29 19:54:11 +0800

    Add files via upload

---
+++ Posting.java
@@ -0,0 +1,41 @@
+import java.util.LinkedList;
+
+public class Posting implements Comparable<Posting> {
+    private int docFreq;
+    private LinkedList<String> postingList;
+
+    public Posting(int docFreq, LinkedList<String> postingList)
+    {
+        this.docFreq = docFreq;
+        this.postingList = postingList;
+    }
+
+    public Posting()
+    {
+    }
+
+    public int getDocFreq() {
+        return docFreq;
+    }
+
+    public void setDocFreq(int docFreq) {
+        this.docFreq = docFreq;
+    }
+
+    public LinkedList<String> getPostingList() {
+        return postingList;
+    }
+
+    public void setdocId(LinkedList<String> postingList) {
+        this.postingList = postingList;
+    }
+    
+    public String toString() {
+    	return docFreq + " => " + postingList.toString();
+    }
+    
+    public int compareTo(Posting p)
+    {
+         return (this.docFreq - p.docFreq);
+    }
+}
